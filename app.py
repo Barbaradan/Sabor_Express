18b5ebfe90eda853c41e11f6be1e11e2ccfda6c7
Barbaradan <barbaradan2003@gmail.com>
2025-11-17 11:14:19 -0300

    feat: criar funções

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-print('Sabor Express\n')
+import os #biblioteca os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_nome_do_programa():
+    print('Sabor Express\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app(): #funtion relacionada ao else
+    os.system('cls') #executa o import, cls limpa o terminal
+    print('Finalizando o app\n')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print(f'Você escolheu a opção {opcao_escolhida}')
+
+    match opcao_escolhida:
+        case 1:
+            print('Cadastrar restaurante')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            finalizar_app()
+        case _:
+            print('Opção invalida!\n')
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
