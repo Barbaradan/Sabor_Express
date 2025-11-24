fdd59eb66120724c83959547cf2dc61fd526a8be
Barbaradan <barbaradan2003@gmail.com>
2025-11-24 14:00:28 -0300

    feat: Adicionar metodos case, try e except. Implementar função main.

--- app.py
+++ app.py
@@ -13,23 +13,32 @@ def finalizar_app(): #funtion relacionada ao else
     os.system('cls') #executa o import, cls limpa o terminal
     print('Finalizando o app\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    print(f'Você escolheu a opção {opcao_escolhida}')
+def opcao_invalida():
+    print('Opção invalida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
-    match opcao_escolhida:
-        case 1:
-            print('Cadastrar restaurante')
-        case 2:
-            print('Listar restaurantes')
-        case 3:
-            print('Ativar restaurante')
-        case 4:
-            finalizar_app()
-        case _:
-            print('Opção invalida!\n')
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print(f'Você escolheu a opção {opcao_escolhida}')
+
+        match opcao_escolhida: #condição match q substitui if/else
+            case 1:
+                print('Cadastrar restaurante')
+            case 2:
+                print('Listar restaurantes')
+            case 3:
+                print('Ativar restaurante')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls') #limpa a tela
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
